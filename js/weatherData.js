dc94b400671f55377e14bd98818895db672b4c6e
ismailgunayy <29682307+ismailgunayy@users.noreply.github.com>
2022-07-03 22:46:01 +0300

    Deleted console.log function

--- js/weatherData.js
+++ js/weatherData.js
@@ -2,8 +2,6 @@ async function getWeatherData({ latitude, longitude }, unit = 'metric') {
 	const API_KEY = '6a9ce81dfa32b9002cbfc77cb080e0b4';
 	const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${unit}`;
 
-	console.log('REQUEST SENT');
-
 	const response = await fetch(API_URL);
 	const weatherData = await response.json();
 	weatherData.date = new Date().toLocaleDateString('en-US', {
